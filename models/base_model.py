143d38b17b423ada9b0d3fffd19b4808ac151991
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-03 17:26:47 +0000

    Create model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,18 @@ class BaseModel:
 
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes the object. """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
 
     def __str__(self):
         """ Returns the string representattion of the object """
