f3f61285e7ca71abac87dfe71af1d3725f2a8cdf
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-07 13:38:01 +0000

    Console

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ make us of the methods and attributes associted with the cmd.Cmd class
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
